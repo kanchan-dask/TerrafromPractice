34099b746debc9c9fc44be0865a1e94282ebfe6c
kanchan-dask <kanchanmeengineer@gmail.com>
2022-11-30 21:04:12 +0530

    Committing main.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+
+   resource_provider {
+       azurerm = {
+              source = "hashicorp/azurerm"
+              version = "3.0.0"
+                 }
+       }
+
+  provider "azurerm" {
+        features {}
+}
+ backend "storeTfState" {
+         resource_group = "RGForPractice"
+	 storage_account = "storageaccountpracticeey"
+         container_name = "containerforpractice"
+         key = "terraform.tfstate"
+     }
+ 
+
+}
+
+ locals {
+      resource_group = "RGForPractice"
+      location = "centralindia"
+}
+
+ resource "azurerm_event_grid_system_topic" "stgTopic" {
+        name = "stgAccountTopicTerra"
+        resource_group = local.resource_group 
+        location = local.location
+        topic_type = "Microsoft.Storage/storageAccounts"
+        source_arm_resource_id = "/subscriptions/abfa7548-7cd5-4ab5-91b2-07efdced45fe/resourceGroups/RGForPractice/providers/Microsoft.Storage/storageAccounts/storageaccountpracticeey"
+        }
+
+ resource "azurerm_storage_queue" "stgQueue" {
+       name = "stgAccountQueueTerra"
+       storage_account_name = "storageaccountpracticeey"
+}
+
+ resource "azurerm_event_grid_system_topic_event_subscription" "eventSub" {
+      name = "eventSubTerra"
+      location = local.location
+      system_topic = azurerm_event_grid_system_topic.stgTopic.id
+      storage_queue_endpoint {
+         storage_account_id = "/subscriptions/abfa7548-7cd5-4ab5-91b2-07efdced45fe/resourceGroups/RGForPractice/providers/Microsoft.Storage/storageAccounts/storageaccountpracticeey"
+         queue_name = azurerm_storage_queue.stgQueue.name
+}
+
+
+ 
